fc68ad2bd6dbb5fa28d44a76979c8e1bd2eb1059
Max Niederman <max@maxniederman.com>
2023-06-11 12:14:34 -0700

    fix(nixos/user): add auth methods

--- modules/nixos/user.nix
+++ modules/nixos/user.nix
@@ -12,10 +12,16 @@
           "networkmanager"
         ];
         shell = pkgs.nushell;
+
+        openssh.authorizedKeys.keys = [
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINmdKg6WzEiyKysklc3YAKLjHEDLZq4RAjRYlSVbwHs9 max@tar-minyatur"
+        ];
       };
     };
 
     security.sudo.wheelNeedsPassword = false;
+
+    services.openssh.enable = true;
 
     # needed for shell completions
     programs.fish.enable = true;
