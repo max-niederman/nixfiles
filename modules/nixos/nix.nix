1232179fd641f134d02b4d730bd15245e87e1bc3
Max Niederman <max@maxniederman.com>
2024-03-15 14:56:14 -0700

    feat(nix): remove old caches and add garnix cache

--- modules/nixos/nix.nix
+++ modules/nixos/nix.nix
@@ -14,14 +14,14 @@
         trusted-users = [ "root" "@wheel" ];
 
         substituters = [
-          "https://niederman.cachix.org"
           "https://nix-community.cachix.org"
           "https://hyprland.cachix.org"
+          "https://cache.garnix.io"
         ];
         trusted-public-keys = [
-          "niederman.cachix.org-1:RHf23lVKJ95Lb/5NEoMTGUUns8zg9/jIHNJXXekUoZc="
           "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
           "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+          "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
         ];
       };
 
