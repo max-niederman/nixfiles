e73b464639cf8e52f93a41006dcf7eea845120f5
Max Niederman <max@maxniederman.com>
2024-04-10 22:20:47 -0700

    switch to single ssh key for all hosts

--- modules/nixos/user.nix
+++ modules/nixos/user.nix
@@ -21,7 +21,7 @@
         inherit (secrets) hashedPassword;
 
         openssh.authorizedKeys.keys = [
-          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINmdKg6WzEiyKysklc3YAKLjHEDLZq4RAjRYlSVbwHs9 max@tar-minyatur"
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINmdKg6WzEiyKysklc3YAKLjHEDLZq4RAjRYlSVbwHs9 max"
         ];
       };
     };
