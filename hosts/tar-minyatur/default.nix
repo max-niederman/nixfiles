996fb858157130c29abacff48e5e1bdf0d6f57a3
Max Niederman <max@maxniederman.com>
2024-04-26 20:50:59 -0700

    update inputs

--- hosts/tar-minyatur/default.nix
+++ hosts/tar-minyatur/default.nix
@@ -36,7 +36,7 @@
     services.xserver.videoDrivers = [ "nvidia" ];
 
     hardware.nvidia = {
-      package = config.boot.kernelPackages.nvidiaPackages.stable;
+      package = config.boot.kernelPackages.nvidiaPackages.beta;
       modesetting.enable = true;
       nvidiaSettings = true;
     };
