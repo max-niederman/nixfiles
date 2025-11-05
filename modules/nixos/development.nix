f6a3d24208874ba77a8de655f4e3310e9ed05fd9
Max Niederman <max@maxniederman.com>
2025-11-05 09:56:33 -0800

    enable nix-ld

--- modules/nixos/development.nix
+++ modules/nixos/development.nix
@@ -6,6 +6,13 @@
 
 {
   config = {
+    programs.nix-ld = {
+      enable = true;
+      libraries = [
+        (pkgs.runCommand "steamrun-lib" {} "mkdir $out; ln -s ${pkgs.steam-run.fhsenv}/usr/lib64 $out/lib")
+      ];
+    };
+
     # for Git signing
     programs.gnupg.agent = {
       enable = true;
