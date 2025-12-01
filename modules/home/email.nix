c410dce114788b6d2b04406821d0868810513d8a
Max Niederman <max@maxniederman.com>
2025-12-01 13:07:47 -0800

    add more email accounts

--- modules/home/email.nix
+++ modules/home/email.nix
@@ -23,6 +23,24 @@
         thunderbird.enable = true;
       };
 
+      mechanize = {
+        address = "max@mechanize.work";
+        realName = "Max Niederman";
+
+        flavor = "gmail.com";
+
+        thunderbird.enable = true;
+      };
+
+      reed = {
+        address = "mniederman@reed.edu";
+        realName = "Max Niederman";
+
+        flavor = "gmail.com";
+
+        thunderbird.enable = true;
+      };
+
       echoprize = {
         address = "max@echoprize.org";
         realName = "Max Niederman";
@@ -37,7 +55,7 @@
       enable = true;
       profiles.default = {
         isDefault = true;
-        accountsOrder = [ "personal" "secondary" "echoprize" ];
+        accountsOrder = [ "personal" "secondary" "mechanize" "reed" "echoprize" ];
       };
     };
   };
