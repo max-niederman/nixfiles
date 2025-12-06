7826cab521d6dea84ad68929eb4da7d8b07c5761
Max Niederman <max@maxniederman.com>
2025-12-05 18:05:20 -0800

    enable containerd and buildkit daemons

--- modules/nixos/development.nix
+++ modules/nixos/development.nix
@@ -9,7 +9,7 @@
     programs.nix-ld = {
       enable = true;
       libraries = [
-        (pkgs.runCommand "steamrun-lib" {} "mkdir $out; ln -s ${pkgs.steam-run.fhsenv}/usr/lib64 $out/lib")
+        (pkgs.runCommand "steamrun-lib" { } "mkdir $out; ln -s ${pkgs.steam-run.fhsenv}/usr/lib64 $out/lib")
       ];
     };
 
@@ -22,12 +22,45 @@
 
     virtualisation.docker = {
       enable = true;
+      extraOptions = "--containerd /run/containerd/containerd.sock";
       autoPrune = {
         enable = true;
         dates = "weekly";
       };
       daemon.settings = {
         max-concurrent-uploads = 12;
+        features.containerd-snapshotter = true;
+      };
+    };
+
+    virtualisation.containerd = {
+      enable = true;
+    };
+
+    # buildkitd isn't in nixos yet unfortunately, so we need to manually configure it
+    systemd.services.buildkit = {
+      description = "Buildkit";
+
+      requires = [ "buildkit.socket" ];
+      after = [ "buildkit.socket" ];
+
+      wants = [ "multi-user.target" ];
+
+      serviceConfig = {
+        Type = "notify";
+        ExecStart = "${pkgs.buildkit}/bin/buildkitd --addr fd:// --oci-worker false --containerd-worker true";
+      };
+    };
+    systemd.sockets.buildkit = {
+      description = "BuildKit";
+
+      wantedBy = [ "sockets.target" ];
+
+      socketConfig = {
+        ListenStream = "%t/buildkit/buildkitd.sock";
+        SocketMode = "0660";
+        SocketUser = "root";
+        SocketGroup = "docker";
       };
     };
 
@@ -42,6 +75,8 @@
 
     environment.systemPackages = [
       pkgs.perf
+      pkgs.
+      pkgs.buildkit
     ];
   };
 }
